1ecbddf42abecafb9959ddc17ea5310078684c2e
AleSR13 <ale.hdz.segura@gmail.com>
2021-03-10 11:50:47 +0100

    Fixing bug with not passing resources to bsub and adding possibility to run local

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -8,17 +8,17 @@ cd ${DIR}
 # Sanity checks
 if [ ! -z "${1}" ] || [ ! -z "${2}" ]
 then
-   INPUTDIR="${1}"
-   OUTPUTDIR="${2}"
+    INPUTDIR="${1}"
+    OUTPUTDIR="${2}"
 else
     echo "no inputdir or outputdir(param 1, 2)"
     exit 1
 fi
 
-if [ ! -d "${INPUTDIR}" ] || [ ! -d "${OUTPUTDIR}" ] || [ ! -d "${INPUTDIR}/trimmomatic" ]
+if [ ! -d "${INPUTDIR}" ] || [ ! -d "${OUTPUTDIR}" ] || [ ! -d "${INPUTDIR}/clean_fastq" ]
 then
-    echo "input dir $INPUTDIR, output dir $OUTPUTDIR or trimmomatic dir ${INPUTDIR}/trimmomatic does not exists"
+    echo "input dir $INPUTDIR, output dir $OUTPUTDIR or clean_fastq dir ${INPUTDIR}/clean_fastq does not exists"
     exit 1
 fi
 
-./juno-salmonella -y -i "${INPUTDIR}/trimmomatic" -o "${OUTPUTDIR}"
+./juno-salmonella -y -i "${INPUTDIR}/clean_fastq" -o "${OUTPUTDIR}"
